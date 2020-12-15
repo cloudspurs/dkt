0bb48d90daafc6bbce4dad39dd622a41441b4e3f
cloud <cloud@email.com>
2020-12-15 10:23:24 +0800

    test.py

--- src/test.py
+++ src/test.py
@@ -1,21 +1,25 @@
-import os 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-os.environ['CUDA_VISIBLE_DEVICES'] = '4'
-
-import numpy as np
-import tensorflow as tf
-
-a = tf.constant([[1, 0, 3], [3, 0, 5]])
-b = tf.constant([[1, 0, 1], [1, 0, 1]])
-c = tf.reduce_sum(a, axis=-1, keepdims=True)
-d = tf.math.count_nonzero(b, axis=-1, keepdims=True)
+#import os 
+#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+#os.environ['CUDA_VISIBLE_DEVICES'] = '4'
+#import tensorflow as tf
+#a = tf.constant([[1, 0, 3], [3, 0, 5]])
+#b = tf.constant([[1, 0, 1], [1, 0, 1]])
+#c = tf.reduce_sum(a, axis=-1, keepdims=True)
+#d = tf.math.count_nonzero(b, axis=-1, keepdims=True)
 
 
-#print(pickle.HIGHEST_PROTOCOL)
-#a = 1
-## 保存
-#with open('data.pickle', 'wb') as f:
-#  pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
-#  # 读取
-#  with open('data.pickle', 'rb') as f:
-#    b = pickle.load(f)
+# dataset.from_generator
+seqs = [0, 1, 2, 3, 4]
+x = lambda: seqs
+print(iter(x()))
+for b in iter(x()):
+	print(b)
+
+def gen():
+	for i in range(5):
+		yield i
+	
+print(iter(gen()))
+for b in iter(gen()):
+	print(b)
+	
