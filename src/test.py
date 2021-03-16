c18ecc1b61f6d075b525f167d42ff683e1b9b35a
cloud <cloud@email.com>
2021-03-16 17:05:32 +0800

    test

--- src/test.py
+++ src/test.py
@@ -1,7 +1,7 @@
 #import os 
 #os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 #os.environ['CUDA_VISIBLE_DEVICES'] = '4'
-#import tensorflow as tf
+import tensorflow as tf
 #a = tf.constant([[1, 0, 3], [3, 0, 5]])
 #b = tf.constant([[1, 0, 1], [1, 0, 1]])
 #c = tf.reduce_sum(a, axis=-1, keepdims=True)
@@ -9,17 +9,19 @@
 
 
 # dataset.from_generator
-seqs = [0, 1, 2, 3, 4]
-x = lambda: seqs
-print(iter(x()))
-for b in iter(x()):
-	print(b)
-
-def gen():
-	for i in range(5):
-		yield i
+#seqs = [0, 1, 2, 3, 4]
+#x = lambda: seqs
+#print(iter(x()))
+#for b in iter(x()):
+#	print(b)
+#
+#def gen():
+#	for i in range(5):
+#		yield i
+#	
+#print(iter(gen()))
+#for b in iter(gen()):
+#	print(b)
 	
-print(iter(gen()))
-for b in iter(gen()):
-	print(b)
-	
+print(tf.__version__)
+print("gpu", tf.test.is_gpu_available())
